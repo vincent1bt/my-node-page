198185f4b6409b316fc56972309c73e0528b67b2
vicente <vincent_97@live.com.mx>
2018-10-04 19:59:41 -0500

    production fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,6 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
       NODE_ENV: 'production'
     }
   }]
